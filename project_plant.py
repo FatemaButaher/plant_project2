31105220c6a99541b89784da85d75599e83563b8
FatemaButaher <fatimya37@gmail.com>
2026-09-22 14:11:41 +0300

    Fix syntax error in filtering growth data

--- project_plant.py
+++ project_plant.py
@@ -93,7 +93,7 @@ def plant_growth():
 
     selected_plant = st.selectbox("Select plant", pf.get_plant_names(data))
 
-    Fillter = growth_data[growth_data["Plant Name"] == selected_plant
+    Fillter = growth_data[growth_data["Plant Name"] == selected_plant]
     result = Fillter[["Date" , "Height"]]
 
     if result.empty:
