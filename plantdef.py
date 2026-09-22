85e48b324f1a3b510dc268f1b6dd57519ba5203a
FatemaButaher <fatimya37@gmail.com>
2026-09-22 13:50:43 +0300

    Update date format in get_record_care_data function
    Change date format from string to date object in new_activity.

--- plantdef.py
+++ plantdef.py
@@ -25,7 +25,7 @@ def add_plant_data(df, plant_name, location, date_acquired, watering_frequency, 
 def get_record_care_data(df, selected_plant, activity_performed):
     today_date = datetime.date.today()
     
-    new_activity = pd.DataFrame([{ "Plant": selected_plant, "Activity": activity_performed, "Date": str(today_date)}])
+    new_activity = pd.DataFrame([{ "Plant": selected_plant, "Activity": activity_performed, "Date": today_date}])
     
     df = pd.concat([df, new_activity], ignore_index=True)
 
